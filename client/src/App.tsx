6997e435412a8693e6dc4bdf728c9145b5a4d4b4
MalekMameri <m.mameri.imm@gmail.com>
2025-07-15 23:31:41 +0100

    Fix GitHub Pages routing: add Wouter base path and 404.html support

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,20 @@
-import { Switch, Route } from "wouter";
+import { Router, Switch, Route } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
 function App() {
   return (
-    <TooltipProvider>
-      <Toaster />
-      <Router />
-    </TooltipProvider>
+    <Router base="/portfolio">
+      <TooltipProvider>
+        <Toaster />
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route component={NotFound} />
+        </Switch>
+      </TooltipProvider>
+    </Router>
   );
 }
 
